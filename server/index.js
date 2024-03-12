6ab03186105cc91ef691fea87446e23c10716dfe
anirudhgeek21 <anirudhprajapati2466@gmail.com>
2024-03-12 12:06:53 +0530

    new backend

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({
     origin: "https://purple-bear-lifestyle.vercel.app",
-    methods: ["POST"],
+    methods: ["POST","GET"],
     credentials: true
   }));
 
